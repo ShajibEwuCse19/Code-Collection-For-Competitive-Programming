c584d105a78225729363f7a48e3e1fe264bebdc7
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2021-01-09 12:01:59 +0600

    Update Diameter-of-a-Tree.cpp
    Diameter of a tree : Longest Path between two nodes.
    Complexity         : O (V + E), same as bfs. Here V is total Nodes and E is total Edges.
    
    We can use bfs or dfs to find longest distanced Node (X) from Root Node(A). Then we can find another longest distanced Node (Y) from (X).
    Distance from X to Y is the Diameter of The Tree.
    The tree can be weighted , unweighted or binary tree.

--- Graph-Theory/Diameter-of-a-Tree.cpp
+++ Graph-Theory/Diameter-of-a-Tree.cpp
@@ -1,45 +1,78 @@
+//Md. Shajibul Islam..
+//East West University, CSE Department'19
 
 #include<bits/stdc++.h>
-#define N ((int)1e5 + 5)
-#define MAXL ((ll)1e18 + 5)
-#define MAX ((int)2e9 + 5)
-#define fastio ios_base::sync_with_stdio(false),cin.tie(NULL)
-#define endl "\n"
-#define ll long long
-
 using namespace std;
+#define FAST             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl             '\n'
+#define loop(i,n)        for(int i=0;i<n;i++)
+#define CASE_PRINT       cout<<"Case "<<C<<": "
+#define CASE_PRINT2      cout<<"Case "<<C<<":"<<endl
+#define ll               long long
+#define ld               long double
+#define Pi               2*acos(0.0) // acos(-1.0)
+int dx[] = {0, 0, +1, -1, +1, +1, -1, -1};
+int dy[] = {+1, -1, 0, 0, +1, -1, +1, -1};
 
-vector < pair < int , int > > vec[N]; //first element is adj node and second element is distance.
+const int mx = 1e5+123;
+vector<pair<int,int>>adj[mx]; //first element is adj node and second element is distance.
 
-//all comment is applicable for first dfs--> to find farthest node(X) from root(A).
+//all comment is applicable for first dfs --> to find farthest node(X) from root(A).
 
-pair < int , int > dfs(int n , int par) //n = root node(A) and par = parent of root node.
+pair<int,int> dfs(int n,int par)
 {
-    pair < int , int > ans = {0,n};
-    for(auto p:vec[n]){
-        int a = p.first , c = p.second ;
-        if(a != par){
-            pair < int , int > tmp = dfs(a,n);
-            tmp.first += c;
-            ans = max(ans , tmp); // distance of farthest node(X).
+    pair<int,int>ans = {0,n};
+
+    for(auto i : adj[n]) //n = root node(A) and par = parent of root node.
+    {
+        int a = i.first, dis = i.second;
+
+        if(a != par)
+        {
+            pair<int,int>tmp = dfs(a,n);
+
+            tmp.first = tmp.first + dis; 
+
+            ans = max(ans,tmp); // distance of farthest node(X).
         }
     }
-    return ans;//dfs will return a pair with most distance node and distance.
+
+    return ans; //dfs will return a pair with most distance node and distance.
 }
 
 int main()
 {
-    int n ;
-    cin>>n;
-    for(int i = 1 ; i<n ; i++){
-        int a , b , c;
-        cin>>a>>b>>c;
-        vec[b].push_back({a,c});
-        vec[a].push_back({b,c});
+    FAST;
+    int n;cin>>n;
+
+    for(int i=1;i<=n;i++)
+    {
+        int u,v,w;cin>>u>>v>>w; // u connect with v with w weight.
+        adj[u].push_back({v,w});
+        adj[v].push_back({u,w});
     }
-    
-    int a = dfs(1,0).second;//to find distance root node(A) to farthest node(X). a = farthest node (X).
-    
-    pair < int , int > ans = dfs(a,0);//to find distance from (X) to (Y)  which is the Diameter of the Tree.
-    return 0;
+
+    int A = dfs(1,0).second; //to find distance root node(A) to farthest node(X). a = farthest node (X).
+
+    pair<int,int>ans = dfs(A,0); //to find distance from (X) to (Y)  which is the Diameter of the Tree.
+
+    cout<<ans.first<<" "<<ans.second<<endl;
 }
+
+/*
+8
+1 2 1
+2 3 1
+2 4 1
+3 5 1
+1 6 1
+6 8 1
+6 7 1
+7 9 1
+
+output = 6,5; 6 = distance 5(X) to 9(Y). 5 = X
+*/
+
+
+
+
