f1878887b34221cbe10d74b63683e5cb33a20862
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2021-04-12 21:31:24 +0600

    Update Prime-Generate-(Sieve-of-Eratosthenes).cpp

--- Number-Theory/Prime-Generate-(Sieve-of-Eratosthenes).cpp
+++ Number-Theory/Prime-Generate-(Sieve-of-Eratosthenes).cpp
@@ -1,4 +1,55 @@
 Code-1:
+=================
+const int mx = 1e7+123;
+bitset<mx> isPrime; ///if boolean array use n memory, bitset will use (n/36) memory. initially all index 0
+vector<int> prime; ///store all prime.
+
+void sieve ( int n )
+{
+    n += 100;///for safety.
+    
+    isPrime[2] = 1;
+    prime.push_back(2);
+    ///all even no composite except 2. loop through only odd no.
+    for ( int i = 3; i <= n; i += 2 ) isPrime[i] = 1;///initially all odd are prime.
+
+    int lim = sqrt(n*1) + 2;///for divisor 
+
+    for ( int i = 3; i <= n; i += 2 ) {
+        if ( isPrime[i] == 1 ) {
+                
+           prime.push_back(i);///if we get (i) it must be prime.
+
+           if(i<=lim)///if we don't do it, (i*i) may overflow.
+           {
+               for ( int j = i*i; j <= n; j += ( i + i ) )isPrime[j] = 0;///cutting all divisor of the prime(i).
+           }
+        }
+    }
+}
+
+int main()
+{
+    ///FAST;
+
+    int n = 100;
+    sieve(n);
+
+    for(int i=1;i<=100; i++)if(isPrime[i]==1)cout<<i<<" ";cout<<endl;///check a no prime or not prime.
+
+    for(auto i : prime)///output from prime vector. all primes stored there.
+    {
+        if(i==0 or i>n)break;
+        cout<<i<<" ";
+    }cout<<endl;
+
+}
+
+Complexity: n * Harmonic Series = nln(n)  ///We can do 1e8 operations in 1sec easily.
+
+
+
+Code-2:
 =============================
 const int mx = 1e6+123;
 bool isPrime[mx];
@@ -34,7 +85,7 @@ int main()
 
 Complexity: n * Harmonic Series = nln(n)  ///We can do 1e8 operations in 1sec easily.
   
- Code-2:
+ Code-3:
 ===================
   
 const int mx = 1e5+123;
