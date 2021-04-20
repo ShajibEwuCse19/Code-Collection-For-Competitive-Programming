1db04df6029a95cc0b2b7bd7f812184399c2eb30
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2021-04-20 12:55:00 +0600

    Update Number-of-Divisors-and-Harmonic-Series.cpp

--- Number-Theory/Number-of-Divisors-and-Harmonic-Series.cpp
+++ Number-Theory/Number-of-Divisors-and-Harmonic-Series.cpp
@@ -149,6 +149,93 @@ Total NOD  =  set2 x set3 x set5 = ৩টি x ২টি x ২টি = ১২ট
 4 -> 4 x 3 x 5 = 60
     
 ALL Divisor, NOD ( 60 ) = 1 , 5 , 3 , 15 , 2 , 10 , 6 , 30 , 4 , 20 , 12 , 60 মোট ১২টি ডিভাইজর ৬০ এর যা Prime Factor of 60 (2 , 3 , 5 ) থেকে নির্ণয় করা হয়েছে । 
+
+Code:
+==========
+//Md. Shajibul Islam..
+//East West University, CSE Department'19
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl             '\n'
+#define loop(i,n)        for(int i=0;i<n;i++)
+#define CASE_PRINT       cout<<"Case "<<C<<": "
+#define CASE_PRINT2      cout<<"Case "<<C<<":"<<endl
+#define ll               long long
+#define ld               long double
+#define Pi               2*acos(0.0) // acos(-1.0)
+#define err              1e-9
+int dx[] = {0, 0, +1, -1, +1, +1, -1, -1};
+int dy[] = {+1, -1, 0, 0, +1, -1, +1, -1};
+
+const int mx = 1e7+123;
+
+vector<int> fact[mx];///store all prime divisor here.
+
+bitset<mx> isPrime; ///if boolean array use n memory, bitset will use (n/32) memory. initially all index 0
+vector<int> prime; ///store all prime.
+
+///sieve Function..
+void sieve ( int n )
+{
+    n += 100;///for safety.
+
+    isPrime[2] = 1;
+    prime.push_back(2);
+    ///all even no composite except 2. loop through only odd no.
+    for ( int i = 3; i <= n; i += 2 ) isPrime[i] = 1;///initially all odd are prime.
+
+    int lim = sqrt(n*1) + 2;///for divisor
+
+    for ( int i = 3; i <= n; i += 2 ) {
+        if ( isPrime[i] == 1 ) {
+
+           prime.push_back(i);///if we get (i) it must be prime.
+
+           if(i<=lim)///if we don't do it, (i*i) may overflow.
+           {
+               for ( int j = i*i; j <= n; j += ( i + i ) )isPrime[j] = 0;///cutting all divisor of the prime(i).
+           }
+        }
+    }
+}
+
+ll NOD (ll n)
+{
+    ll Total_NOD = 1; /// for multiply initial NOD = 1
+
+    for ( auto p : prime ) {
+        if ( 1LL * p * p > n ) break;
+
+        if ( n % p == 0 ) {
+            int cnt = 1; ///cnt = 1 for (d^0 = 1) 
+
+            while ( n % p == 0 ) {
+                n /= p;
+                cnt++; /// cnt total divisor of a specific divisor of a number. if n = 60 then for 2 , cnt = 1 + 1 + 1 = 3.
+            }
+
+            Total_NOD = ( cnt * Total_NOD ); /// Total_NOD = (3 * 1) = 3 for n = 60 and divisor = 2;
+        }
+    }
+
+    if ( n > 1 ) Total_NOD = ( 2 * Total_NOD ); ///it will increase 2 times. coz ( 1 * sometnig ) = somethig 
+
+    return Total_NOD; 
+}
+
+int main()
+{
+    FAST;
+    sieve(1e7); ///it'll work for (1e14).
+
+    ll n = 60;
+    ll ans = NOD(n);
+
+    cout<<ans<<endl;///3 x 2 x 2 = 12 for n = 60
+}
+
     
 
 
