a2e6ef122a65c91b36d8b88f3cf3befe8c3bd5e9
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2021-05-04 15:32:05 +0600

    Update Sum-of-Divisor-(SOD).cpp

--- Number-Theory/Sum-of-Divisor-(SOD).cpp
+++ Number-Theory/Sum-of-Divisor-(SOD).cpp
@@ -6,10 +6,10 @@ Sum of Divisor, SOD(12) = 1 + 2 + 3 + 4 + 6 + 12 = 28
 Now, Prime Factor of 12 = 2 , 2 , 3 কারণ (2 x 2 x 3 = 12 )
                         = (2^2 , 3^1)
   
-    SOD(12)             = sum of all combination of  (2^2 , 3^1)
+    SOD(12)             = sum of all combination of  (2^2 , 3^1) /// Details in NOD tutorial. 
                         = (2^0 x 3^0) + (2^1 x 3^0) + (2^2 x 3^0) + (2^0 x 3^1) + (2^1 x 3^1) + (2^2 x 3^1) = 1 + 2 + 4 + 3 + 6 + 12 = 28
                         = 2^0 (3^0 + 3^1) + 2^1 (3^0 + 3^1) + 2^2 (3^0 + 3^1) -> Common numbers 
-                        = (3^0 + 3^1) x (2^0 + 2^1 + 2^2) -> Common (3^0 + 3^1) the we'll get the common pattern.
+                        = (3^0 + 3^1) x (2^0 + 2^1 + 2^2) -> Common (3^0 + 3^1) then we'll get the common pattern.
                         = (1 + 3 ) x ( 1 + 2 + 4 )
                         = 4 x 7
                         = 28 (From Prime Factorization).
