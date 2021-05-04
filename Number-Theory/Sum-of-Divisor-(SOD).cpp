7eb7a0e56986b1dbb81e00e84c04f774cad57884
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2021-05-04 16:56:42 +0600

    Update Sum-of-Divisor-(SOD).cpp

--- Number-Theory/Sum-of-Divisor-(SOD).cpp
+++ Number-Theory/Sum-of-Divisor-(SOD).cpp
@@ -1,3 +1,94 @@
+Code (Fast):
+==============
+//Md. Shajibul Islam..
+//East West University, CSE Department'19
+
+#include<bits/stdc++.h>
+using namespace std;
+#define FAST             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl             '\n'
+#define loop(i,n)        for(int i=0;i<n;i++)
+#define CASE_PRINT       cout<<"Case "<<C<<": "
+#define CASE_PRINT2      cout<<"Case "<<C<<":"<<endl
+#define ll               long long
+#define ld               long double
+#define Pi               2*acos(0.0) // acos(-1.0)
+#define err              1e-9
+int dx[] = {0, 0, +1, -1, +1, +1, -1, -1};
+int dy[] = {+1, -1, 0, 0, +1, -1, +1, -1};
+
+const int mx = 1e7+123;
+
+bitset<mx> isPrime; ///if boolean array use n memory, bitset will use (n/32) memory. initially all index 0
+vector<int> prime; ///store all prime.
+
+///sieve Function..
+void sieve ( int n )
+{
+    n += 100;
+
+    isPrime[2] = 1;
+    prime.push_back(2);
+
+    for ( int i = 3; i <= n; i += 2 ) isPrime[i] = 1;
+
+    int lim = sqrt(n*1) + 2;
+
+    for ( int i = 3; i <= n; i += 2 ) {
+        if ( isPrime[i] == 1 ) {
+
+           prime.push_back(i);
+
+           if(i<=lim)
+           {
+               for ( int j = i*i; j <= n; j += ( i + i ) )isPrime[j] = 0;
+           }
+        }
+    }
+}
+
+ll SOD (ll n)
+{
+    ll ret = 1; ///Total sum.
+
+    for ( auto p : prime ) {
+        if ( 1LL * p * p > n ) break;
+
+        if ( n % p == 0 ) {
+
+                ll pawP = p;
+
+            while ( n % p == 0 ) {
+
+                pawP   = ( pawP * p ) ; ///p^1 x p^1 = p^2 x p^1 = p^3 x p^1 = p^4.......Here, p means p^1
+
+                n /= p;
+            }
+
+            ret = ( ret * (( pawP -1 ) / ( p - 1 ) ) ) ; /// common pattern = a.( r^(n+1) - 1 ) / r - 1 ) Here, a = p^0 = 1 and powP = n + 1 + 1 and ret = r;
+        }
+    }
+
+    if ( n > 1 ) ret = ret * ( 1 + n ); /// ( 1 + n ) = p^0 + p^1 . Here, P^1 = remaining part of n which greater than 1.
+
+    return ret;
+}
+
+int main()
+{
+    FAST;
+    ll lim = 1e7;
+    sieve(lim); ///it'll work for (1e14).
+
+    ll n = 12;
+    cout<<SOD(n)<<endl;
+}
+
+Complexity: Complexity of Sive + Prime Factorization = (sqrt(n) / ln(sqrt(n))) + log(n). [2 based log] 
+  
+  
+===================================================================================================================================================================
+
 Let, n = 12;
 
 Divisors of 12 = 1 2 3 4 6 12
@@ -26,7 +117,7 @@ Let, SOD(x) = (a , b) x (c , d) -> Common pattern Let, a = 1, b = 2, c = 3, d = 
             = (ac + ad) + (bc + bd)              = (1x3 + 1x4 + 2x3 + 2x4) 
             = SOD(x)                             = 3 + 4 + 6 + 8 = 21
   
-  
+=====================================================================================================================================================================
   
 Code:
 =====
@@ -117,7 +208,7 @@ int main()
 }
 
 
-Complexity: Complexity of Sive + Prime Factorization = (sqrt(n) / ln(sqrt(n))) + log(n). [2 based log] 
+Complexity: same as before.
 
             
 
