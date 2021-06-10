cc810375a361201532fda0a0746340a7d8e4fc2e
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2021-06-11 03:31:23 +0600

    Update Euler-Phi-Or-Divisor-Extension-Theorem.cpp

--- Number-Theory/Euler-Phi-Or-Divisor-Extension-Theorem.cpp
+++ Number-Theory/Euler-Phi-Or-Divisor-Extension-Theorem.cpp
@@ -1,7 +1,7 @@
 Theory:
 =========
 মনে করি একটি সংখ্যা N এবং d হচ্ছে N এর যে কোন একটি ডিভাইজর যার রেঞ্জ 1 to N এর মধ্যে। এখন এমন কতগুলো সংখ্যা আছে রেঞ্জ 1 to N এর মধ্যে যাদের GCD ( a , N ) = d হয় , এখনে a হচ্ছে 
-1 to N এর মধ্যে যে কোন সংখ্যা। এর উত্তর হবে ϕ ( N / d ) .
+1 to N এর মধ্যে যে কোন সংখ্যা। এর উত্তর হবে ϕ ( N / d ) . অর্থ্যাৎ মোট কতগুলো ডিভাইজর আছে d এর সমান তা নির্ণয় করতে হবে।
 Given a number N, let d be a divisor of N. Then the number of pairs a , N , where 1 ≤ a ≤ N and gcd( a , N ) = d , is ϕ ( N / d ).
   
 Example: Let, N = 12 , Divisor, d = 3 , a = 1 ≤ a ≤ 12
