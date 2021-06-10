757b6e5f46fb23066d675b75369f5444f0143349
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2021-06-10 12:50:16 +0600

    Update Euler-Phi.cpp

--- Number-Theory/Euler-Phi.cpp
+++ Number-Theory/Euler-Phi.cpp
@@ -1,3 +1,75 @@
+Code:
+===========
+const int mx = 1e7+123 ;
+bitset<mx> isPrime ; ///if boolean array use n memory, bitset will use (n/32) memory. initially all index 0
+vector<int> prime ; ///store all prime.
+
+void sieve ( int n )
+{
+    n += 100 ;///for safety.
+
+    isPrime[2] = 1 ;
+    prime.push_back(2) ;
+    ///all even no composite except 2. loop through only odd no.
+    for ( int i = 3 ; i <= n ; i += 2 ) isPrime[i] = 1;///initially all odd are prime.
+
+    int lim = sqrt( n * 1 ) + 2 ;///for divisor
+
+    for ( int i = 3 ; i <= n ; i += 2 ) {
+        if ( isPrime[i] == 1 ) {
+
+           prime.push_back(i) ;///if we get (i) it must be prime.
+
+           if( i <= lim )///if we don't do it, (i*i) may overflow.
+           {
+               for ( int j = i*i ; j <= n ; j += ( i + i ) )isPrime[j] = 0;///cutting all divisor of the prime(i).
+           }
+        }
+    }
+}
+
+int Phi ( int n )
+{
+    int ret = n ; /// Global Formula, Phi ( n ) = n x ( ( P - 1 ) / P ) x ( ( Q - 1 ) / Q ) x ( ( R - 1 ) / R ) .... where P,Q,R are Prime Numbers and n = ret.
+
+    for ( auto p : prime )
+    {
+        if ( 1LL * p * p > n and n == 0 ) break;
+
+        if ( n % p == 0 ) /// Phi ( n ) = p1^a X p2^b X p3^c X -----> Create base and power.
+        {
+            ret = ( ret / p ) ;
+            ret = ret * ( p - 1 ) ; /// N x ( ( P - 1 ) ) / P = ( N / P ) x ( P - 1 ) Where P is Prime.
+
+            while ( n % p == 0 ) n = ( n / p ) ;
+        }
+    }
+
+    if ( n > 1 )
+    {
+        ret = ( ret / n ) ;
+        ret = ret * ( n - 1 ) ;
+    }
+
+    return ret ;
+}
+
+int main()
+{
+    FAST;
+    sieve( 1e6 ) ;
+
+    int n = 120 ;
+
+    cout << Phi ( n ) <<endl ; /// ans = 32 for n = 120 ;
+}
+
+
+Complexity: Complexity of Prime Fact = O ( sqrt( n ) / ln ( sqrt ( n ) ). 
+
+
+
+
 Theory Part:
 =================
 Phi ( N ) = Number of Total Co-Prime of Relatively Prime of N in renge 1 to N. 
@@ -6,8 +78,8 @@ Co-Prime or Relatively Prime :
 Let, x and y are two numbers. x and y will be Co-Prime or Relatively Prime if GCD ( x , y ) = 1. That's mean, there is no common divisor of x and y. 
   
 Phi ( a x b x c x ----- x N ) = Phi ( a ) x Phi ( b ) x Phi ( c ) x ----------------- Phi ( N ). So, Phi is a multiplicative function.
+    
   
-
 If, N is a Prime Number, Phi ( N ) = ? 
 All numbers are Co-Prime except N because, GCD ( N , N ) = N. but, GCD ( N , 1 to ( N -1 ) ) = 1 where N is Prime.
 Phi ( N ) = N - 1 ( All numbers except N where N is Prime ).
