b9218d647d165692c52d6ebfa27f75f7ad0a7c9e
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2021-06-10 17:48:45 +0600

    Create Euler-Phi-Extension-Theorem.cpp

---
+++ Number-Theory/Euler-Phi-Extension-Theorem.cpp
@@ -0,0 +1,5 @@
+Theory:
+=========
+মনে করি একটি সংখ্যা N এবং d হচ্ছে N এর যে কোন একটি ডিভাইজর যার রেঞ্জ 1 to N এর মধ্যে। এখন এমন কতগুলো সংখ্যা আছে রেঞ্জ 1 to N এর মধ্যে যাদের GCD ( a , N ) = d হয় , এখনে a হচ্ছে 
+1 to N এর মধ্যে যে কোন সংখ্যা।
+Given a number N, let d be a divisor of N. Then the number of pairs a , N , where 1 ≤ a≤ N and gcd( a , N ) = d , is ϕ ( N / d ).
