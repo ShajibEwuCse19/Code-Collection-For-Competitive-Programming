d483f76beb4772e62efd9fb1e616ed25bbd77704
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2021-07-05 23:16:23 +0600

    Update 3-Single-Linked-List.cpp

--- CSE207/3-Single-Linked-List.cpp
+++ CSE207/3-Single-Linked-List.cpp
@@ -22,7 +22,6 @@ int insert_item(int item) ///insert at the beginning
 	struct node* new_node ; ///stores the address of the new node
 	new_node = (struct node*) malloc (sizeof(struct node)) ;
 	new_node->item = item;
-	new_node->next = 0;
 
     ///step-2: update the pointers of the new node
 	new_node->next = head ; ///point to previous first node
@@ -71,6 +70,64 @@ struct node* search_item(int item)
 	return 0 ;
 }
 
+int length_of_the_list()
+{
+    struct node *temp;
+    temp = head;
+
+    int len = 0;
+    while(temp != NULL)
+    {
+        len++;
+        temp = temp->next;
+    }
+
+    return len;
+}
+///remove the linked list or remove all items
+void remove_the_list()
+{
+    struct node *temp;
+    temp = head;
+
+    while(temp != NULL)
+    {
+        int n = temp->item;
+        delete_item(n);
+
+        temp = temp->next;
+    }
+}
+
+///another way to remove the list or remove all items
+void remove_the_list_using_free()
+{
+    struct node *temp;
+    temp = head;
+
+    while(temp != NULL)
+    {
+        free(temp);
+        temp = temp->next;
+    }
+}
+
+///find the item of the given index from a linked list.
+int get_Nth_item(int index)
+{
+    int len = length_of_the_list();
+    if( index < 0 or index > len )return NULL_VALUE; ///no such index like that in the list
+
+    struct node *temp = head;
+    int cnt = 0;
+    while(temp != NULL)
+    {
+        cnt++;
+        if(cnt == index)return temp->item;
+        temp = temp->next;
+    }
+}
+
 void print_list()
 {
     struct node *temp;
@@ -97,26 +154,39 @@ int main(){
     cout<<"Current List = ";
     print_list();
 
-    cout<<"List after delete last element 10 = ";
-    delete_item(10);///last item
-    print_list();
+    //cout<<"List after delete last element 10 = ";
+    //delete_item(10);///last item
+    //print_list();
 
-    cout<<"List after delete first element 60 = ";
-    delete_item(60);///first item
-    print_list();
+    //cout<<"List after delete first element 60 = ";
+    //delete_item(60);///first item
+    //print_list();
 
-    cout<<"List after delete mid element 30 = ";
-    delete_item(30);///any mid element
-    print_list();
+    //cout<<"List after delete mid element 30 = ";
+    //delete_item(30);///any mid element
+    //print_list();
 
-    cout<<"Try to delete element 99 which is not contain in the list = ";
-    delete_item(99); ///item not found
-    print_list(); cout<<"The list is unchanged "<<endl<<endl<<endl;
+    //cout<<"Try to delete element 99 which is not contain in the list = ";
+    //delete_item(99); ///item not found
+    //print_list(); cout<<"The list is unchanged "<<endl<<endl<<endl;
 
 
-    cout<<"Search item 20"<<endl;
-    struct node *n = search_item(20);
-    cout<<"Return Value = "<<n->item<<endl;
-    if(n->item == 20)cout<<"20 found in the list"<<endl;
+    //cout<<"Search item 20"<<endl;
+    //struct node *n = search_item(20);
+    //cout<<"Return Value = "<<n->item<<endl;
+    //if(n->item == 20)cout<<"20 found in the list"<<endl<<endl<<endl;
+
+    //cout<<"Length or no of item in the list = "<<length_of_the_list();
+    //cout<<"   Value = ";
+    //print_list();
+    //cout<<endl<<endl;
+
+    //cout<<"After Remove the list is = ";
+    //remove_the_list(); /// using self made function.
+    //remove_the_list_using_free();///using free() function
+    //print_list();
+    //cout<<endl<<endl<<endl;
+
+    //cout<<"Element in index 2 = "<<get_Nth_item(2)<<endl;
 
 }
