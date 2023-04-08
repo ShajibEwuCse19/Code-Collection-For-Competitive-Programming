60cbcfd85b9e8899470a6324a5109e083e9fd208
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2023-04-08 20:14:44 +0600

    Update Dijkstra-Shortest-Path-Algo.cpp

--- Graph-Theory/Dijkstra-Shortest-Path-Algo.cpp
+++ Graph-Theory/Dijkstra-Shortest-Path-Algo.cpp
@@ -1,3 +1,117 @@
+//Md. Shajibul Islam..
+//East West University, CSE Department'19
+
+#include<bits/stdc++.h>
+using namespace std;
+
+typedef long long ll;
+typedef vector<int> vi;
+typedef vector<ll> vl;
+typedef vector<vi> vvi;
+typedef vector<vl> vvl;
+typedef pair<int,int> pii;
+typedef pair<double, double> pdd;
+typedef pair<ll, ll> pll;
+typedef vector<pii> vii;
+typedef vector<pll> vll;
+typedef vector<int>::iterator vit;
+typedef set<int>::iterator sit;
+
+#define FAST             ios_base::sync_with_stdio(0); cin.tie(0); cout.tie(0)
+#define endl             '\n'
+#define loop(i,n)        for(int i=0;i<n;i++)
+#define CASE_PRINT       cout<<"Case "<<C<<": "
+#define CASE_PRINT2      cout<<"Case "<<C<<":"<<endl
+#define ll               long long
+#define ld               long double
+#define Pi               2*acos(0.0) // acos(-1.0)
+#define err              1e-9
+
+#define PB push_back
+#define F first
+#define S second
+#define MP make_pair
+#define all(a) (a).begin(),(a).end()
+#define mid(l,r) ((r+l)/2)
+#define left(node) (node*2)
+#define right(node) (node*2+1)
+#define mx_int_prime 999999937
+#define mem(a,b) memset(a, b, sizeof(a) )
+#define gcd(a,b) __gcd(a,b)
+#define sqr(a) ((a) * (a))
+
+
+template < typename F, typename S >
+ostream& operator << ( ostream& os, const pair< F, S > & p ) {
+            return os << "(" << p.first << ", " << p.second << ")";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const vector< T > &v ) {
+            os << "{";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "}";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const set< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename T >
+ostream &operator << ( ostream & os, const multiset< T > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << *it;
+                                            }
+                    return os << "]";
+}
+
+template < typename F, typename S >
+ostream &operator << ( ostream & os, const map< F, S > &v ) {
+            os << "[";
+                for(auto it = v.begin(); it != v.end(); ++it) {
+                                if( it != v.begin() ) os << ", ";
+                                        os << it -> first << " = " << it -> second ;
+                                            }
+                    return os << "]";
+}
+
+#define dbg(args...) do {cerr << #args << " : "; faltu(args); } while(0)
+
+void faltu () {
+            cerr << endl;
+}
+
+template <typename T>
+void faltu( T a[], int n ) {
+            for(int i = 0; i < n; ++i) cerr << a[i] << ' ';
+                cerr << endl;
+}
+
+template <typename T, typename ... hello>
+void faltu( T arg, const hello &... rest) {
+            cerr << arg << ' ';
+                faltu(rest...);
+}
+
+const double PI = acos(-1.0);
+const double eps = 1e-9;
+const int inf = 2000000000;
+const ll infLL = 9000000000000000000;
+#define MOD 1000000007
+int dx[] = {0, 0, +1, -1, +1, +1, -1, -1};
+int dy[] = {+1, -1, 0, 0, +1, -1, +1, -1};
+
 const int mx = 1e5+123;
 vector<pair<int,int>>adj[mx];///node->{weight,adj node}
 int vis[mx];
@@ -21,16 +135,16 @@ void dijkstra(int src)
         if(vis[u])continue;
         vis[u] = 1;
 
-        for(auto v : adj[u])
+        for(auto i : adj[u])
         {
-            int node = v.first; ///adj node
-            int curD = v.second;///distance -> u to b
+            int v = i.first; ///adj node
+            int w = i.second;///distance -> u to b ( Source to
 
-            if(dis[u] + curD < dis[node])  /// current node + distance < already calculated distance -> got a min distance, So, update it
+            if(dis[u] + w < dis[v]) /// source to u cost = dis[u]. u to v cost (dis[u) + w) and dis[v]. Choose the min cost.
             {
-                dis[node] = dis[u] + curD;
+                dis[v] = dis[u] + w;
 
-                pq.push({-dis[node] , node});
+                pq.push({-dis[v] , v});
             }
         }
     }
@@ -68,3 +182,5 @@ Distance 1 to 3 is =>9
 Distance 1 to 4 is =>11
 Distance 1 to 5 is =>11
 */
+
+
