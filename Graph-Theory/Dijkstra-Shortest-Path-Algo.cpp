217769c89054fd16f7138b4dec281a8110a63060
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2023-04-08 20:56:53 +0600

    Update Dijkstra-Shortest-Path-Algo.cpp

--- Graph-Theory/Dijkstra-Shortest-Path-Algo.cpp
+++ Graph-Theory/Dijkstra-Shortest-Path-Algo.cpp
@@ -1,37 +1,37 @@
 const int mx = 1e5+123;
-vector<pair<int,int>>adj[mx];///node->{weight,adj node}
-int vis[mx];
-int dis[mx];///level or distance count.
+vector<pair<ll,ll>>adj[mx];///node->{weight,adj node}
+ll vis[mx];
+ll dis[mx];///level or distance count.
 int n,m;
 
 void dijkstra(int src)
 {
     memset(vis,0,sizeof vis);
-    for(int i=0;i<=n;i++)dis[i] = INT_MAX; /// Initialization
+    for(int i=0;i<=n;i++)dis[i] = infLL; /// Initialization
     dis[src] = 0; /// distance of src is zero
 
-    priority_queue<pair<int,int>>pq; /// first check the min one. 
+    priority_queue<pair<ll,ll>>pq; /// first check the min one.
     pq.push({0,src});
 
     while(!pq.empty())
     {
         int u = pq.top().second;
-        int curD = pq.top().first;
+        ll curD = pq.top().first;
         pq.pop();
 
-        if(vis[u] or dis[u]<curD)continue; /// we don't need to check if we already [using another path] have the min result. 
+        if(vis[u] or dis[u]<curD)continue; /// we don't need to check if we already [using another path] have the min result.
         vis[u] = 1;
 
         for(auto i : adj[u])
         {
             int v = i.first; ///adj node
-            int w = i.second;///distance -> u to b ( Source to
+            ll w = i.second;///distance -> u to b ( Source to
 
             if(dis[u] + w < dis[v]) /// source to u cost = dis[u]. u to v cost (dis[u) + w) and dis[v]. Choose the min cost.
             {
                 dis[v] = dis[u] + w;
 
-                pq.push({-dis[v] , v}); /// cost negative beacuse pq sort decreasing order which is increasing in positive form. 
+                pq.push({-dis[v] , v}); /// cost negative beacuse pq sort decreasing order which is increasing in positive form.
             }
         }
     }
@@ -69,6 +69,3 @@ Distance 1 to 3 is =>9
 Distance 1 to 4 is =>11
 Distance 1 to 5 is =>11
 */
-
-///Complexity : O (E + V * log (V)) base 2 || BFS complexity O (V + E) but in Dijkstra we used Priority queue which works on log(n) base 2. 
-/// Dijkstra is the fastest algo for determine single source and weighted shortest path.
