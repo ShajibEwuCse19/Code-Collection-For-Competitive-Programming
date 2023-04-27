fb8d5d188df3c103fdf253f1dd7419cc9aa3c556
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2023-04-28 00:37:05 +0600

    Update Binary_Search_Tree.cpp

--- CSE207/Binary_Search_Tree.cpp
+++ CSE207/Binary_Search_Tree.cpp
@@ -143,3 +143,10 @@ int main(){
       printf("\nElement not found");
    return 0;
 }
+
+
+A Binary Search Tree (BST) is a tree in which all the nodes follow the below-mentioned properties −
+
+The left sub-tree of a node has a key less than or equal to its parent node's key.
+
+The right sub-tree of a node has a key greater than or equal to its parent node's key.
