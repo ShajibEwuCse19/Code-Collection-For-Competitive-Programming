48cf8a7ec56f21c114c73c53dfe8a72b0df52bfb
Md. Shajibul Islam <36180581+ShajibEwuCse19@users.noreply.github.com>
2023-04-29 22:04:33 +0600

    Update DFS-Adjacency-Matrix.cpp

--- Graph-Theory/DFS-Adjacency-Matrix.cpp
+++ Graph-Theory/DFS-Adjacency-Matrix.cpp
@@ -20,3 +20,32 @@ void dfs(int x, int y)
             dfs(x1,y1);
     }
 }
+
+
+For Leetcode:
+=============
+    int mx = 12;
+    int a[12][12];
+    int n,m;
+    int cnt;
+    bool vis[12][12];
+    
+    void dfs(int x, int y)
+    {
+        int dx[] = {-1,1,0,0};
+        int dy[] = {0,0,-1,1};
+        vis[x][y] = 1;
+
+        for(int i=0; i<4; i++)
+        {
+            int x1 = x + dx[i];
+            int y1 = y + dy[i];
+
+            if(x1>=0 and x1<n and y1>=0 and y1<m and !vis[x1][y1] and a[x1][y1]>0)
+            {
+                cnt = cnt + a[x1][y1];
+                
+                dfs(x1,y1);
+            }
+        }
+    }
